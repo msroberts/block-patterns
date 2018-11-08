3fdee1f565d043a61f3a16a73504aaadcc8105e4
Madeleine Roberts <msroberts@users.noreply.github.com>
2018-11-08 06:29:43 -0500

    Add shape

--- src/patterns/bodice-shaped/back.ts
+++ src/patterns/bodice-shaped/back.ts
@@ -1,4 +1,4 @@
-import { IModel, IModelMap } from 'makerjs'
+import { IModel, IModelMap, models } from 'makerjs'
 import { BodiceBack } from '../bodice-block/back'
 import { IBodiceBlockShaped } from './block'
 
@@ -10,14 +10,32 @@ export interface IBodiceBackShaped extends IModelMap {
   neckline: IModel,
   shoulderDart: IModel,
 }
+
 export class BodiceBackShaped implements IModel {
   public models: IBodiceBackShaped
 
   constructor (block: IBodiceBlockShaped) {
+    const {
+      centerBack,
+    } = block.bodiceBlock.x
+    const {
+      O,
+      linexB,
+    } = block.bodiceBlock.y
+    const {
+      WP,
+    } = block.bodiceBlock.points
+
     const back = new BodiceBack(block.bodiceBlock)
 
     this.models = {
       ...back.models,
+      centerBack: new models.ConnectTheDots(false, [
+        [centerBack, O],
+        [centerBack, linexB],
+        block.points.centerBackInner,
+        WP,
+      ]),
     }
   }
 }
