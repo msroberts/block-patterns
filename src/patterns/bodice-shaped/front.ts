a8a5b76e177bdc35218ac864c42760fe301bf5d6
Madeleine Roberts <msroberts@users.noreply.github.com>
2018-11-10 07:17:48 -0500

    add shapes

--- src/patterns/bodice-shaped/front.ts
+++ src/patterns/bodice-shaped/front.ts
@@ -1,4 +1,5 @@
-import { IModel, IModelMap } from 'makerjs'
+import { IModel, IModelMap, models } from 'makerjs'
+import { smoothCurve } from '../../helpers/curve'
 import { BodiceFront } from '../bodice-block/front'
 import { IBodiceBlockShaped } from './block'
 
@@ -17,8 +18,44 @@ export class BodiceFrontShaped implements IModel {
   constructor (block: IBodiceBlockShaped) {
     const front = new BodiceFront(block.bodiceBlock)
 
+    const {
+      centerFront,
+    } = block.bodiceBlock.x
+    const {
+      neckline,
+      lineC,
+    } = block.bodiceBlock.y
+    const {
+      WPf,
+      UPf,
+    } = block.bodiceBlock.points
+    const {
+      underarmAngle,
+    } = block.bodiceBlock.angles
+    const {
+      centerFrontInner,
+    } = block.points
+
     this.models = {
       ...front.models,
+      centerFront: new models.ConnectTheDots(false, [
+        [centerFront, neckline],
+        [centerFront, lineC],
+        centerFrontInner,
+        WPf,
+      ]),
+      underarm: smoothCurve([
+        {
+          angleInDegrees: underarmAngle,
+          distance: (UPf[1] - WPf[1]) / 3,
+          origin: UPf,
+        },
+        {
+          angleInDegrees: 270,
+          distance: (UPf[1] - WPf[1]) / 3,
+          origin: WPf,
+        },
+      ]),
     }
   }
 }
