054376850539d0364fd1a568ac2b948e7251bd43
Madeleine Roberts <msroberts@users.noreply.github.com>
2018-11-11 07:22:23 -0500

    make dots smaller

--- src/helpers/dots.ts
+++ src/helpers/dots.ts
@@ -4,7 +4,7 @@ export function dots (points: IPoint[], startIndex: number = 0): IModel {
   const model: IModel = { paths: {} }
 
   for (let i = 0; i < points.length;i++) {
-    model.paths![`dot${i + startIndex}`] = new paths.Circle(points[i], 0.25)
+    model.paths![`dot${i + startIndex}`] = new paths.Circle(points[i], 0.1)
   }
 
   return model
